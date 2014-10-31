4536763851147c01c0e6b3f888186750c6037a1b
Ultrabenosaurus <naebeth@googlemail.com>
2014-10-31 16:32:44 +0000

    fix .conf check

--- server/config.php
+++ server/config.php
@@ -5,4 +5,4 @@ define( "PHP_DDNS_ROOT", dirname( __FILE__ ) . "/" );
 require_once PHP_DDNS_ROOT . "Core/PHP_DDNS_Helper.php";
 \PHP_DDNS\Core\PHP_DDNS_Helper::registerLoader();
 
-$_config = ( ( file_exists( PHP_DDNS_ROOT . "assets/.conf" ) ) ? json_decode( file_get_contents( PHP_DDNS_ROOT . "assets/other/.conf" ), true ) : \PHP_DDNS\Core\PHP_DDNS::getDefaults() );
+$_config = ( ( file_exists( PHP_DDNS_ROOT . "assets/other/.conf" ) ) ? json_decode( file_get_contents( PHP_DDNS_ROOT . "assets/other/.conf" ), true ) : \PHP_DDNS\Core\PHP_DDNS::getDefaults() );
